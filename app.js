ad3318e4767559d52bdb77ffeb3f461f9823ec62
tuliogomide <tuliogomide@icloud.com>
2020-12-06 10:53:06 -0300

    identação das rotas

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var app             = require('./v1/config/app_config');
-var db              = require('./v1/config/db_config');
-var usuario         = require('./v1/routes/usuarioRouter');
+var app = require('./v1/config/app_config');
+var db = require('./v1/config/db_config');
+var usuario = require('./v1/routes/usuarioRouter');
 var postoController = require('./v1/controllers/postoController');
-var postos          = require('./v1/routes/postoRouter');
+var postos = require('./v1/routes/postoRouter');
 
 
-app.get('/', function(req,res){
-	res.end("Bem-vindo a API de postos");
+app.get('/', function (req, res) {
+  res.end("Bem-vindo a API de postos");
 
 });
 

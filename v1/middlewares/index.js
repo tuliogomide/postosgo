1989962a1a58e9dea7541e43b499a3b1784c101d
tuliogomide <tuliogomide@icloud.com>
2020-12-17 21:49:03 -0300

    organização de identação

--- v1/middlewares/index.js
+++ v1/middlewares/index.js
@@ -1,19 +1,19 @@
 var jwt = require('jsonwebtoken');
 
 exports.authenticateJWT = function (req, res, next) {
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
-        jwt.verify(token, 'fbbdf33639da90cc80adf56732c41cb30772b61c', (err, decoded) => {
-            if (err) {
-               return res.status(401).json({ erro: true, mensagem: err.message });
-            }
+    jwt.verify(token, 'fbbdf33639da90cc80adf56732c41cb30772b61c', (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ erro: true, mensagem: err.message });
+      }
 
-            next();
-        });
-    } else {
-       return res.status(401).json({ erro: true, mensagem: 'Token é requerido' });
-    }
+      next();
+    });
+  } else {
+    return res.status(401).json({ erro: true, mensagem: 'Token é requerido' });
+  }
 };
